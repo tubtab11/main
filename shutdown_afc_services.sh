979641e315738fe94a29b46aab3774cc52857e9b
tubtab11 <nattaponkuntaisongg@gmail.com>
2022-05-19 17:11:03 +0700

    yes

--- shutdown_afc_services.sh
+++ shutdown_afc_services.sh
@@ -74,7 +74,7 @@ item=`nodehealth.sh|grep "Node Type" | nawk '{print $5}'|cut -c 2-4`
     
         if [ $Mode == "force" ]; 
         then
-            echo "Mode stop force" >> $LOG1
+            echo "$(date +"%Y%m%d%H%M%S") : Mode stop force" >> $LOG1
             check_ps
             shutdown_status
             
